5c9aa36f22f980ca3c8518e4144ef84e559c3fcf
Sachin Pokharel <sachinwork33@gmail.com>
2022-04-18 19:58:48 +0200

    added links

--- fashlance/routes.py
+++ fashlance/routes.py
@@ -1,4 +1,5 @@
-from flask import render_template, redirect, request
+from email.policy import default
+from flask import render_template, redirect, request, session
 from fashlance import app
 from fashlance.utils import *
 
@@ -11,15 +12,29 @@ def index():
 
 @app.route('/technologies', methods=['GET', 'POST'])
 def technologies():
-    if request.method == 'POST':
-        name = request.form.get('name')
+        name = session.get("keyword", None)
+      #  print(name)
         countries = all_nations()
         for x, y in zip(countries['id'], countries['name']):
-            if y == name:
-                df = get_data('SP.DYN.LE00.IN', x)
-                df = df.dropna()
+            df, _ = get_data(x)
                 
         return render_template('technologies.html', year=df['year'].tolist(), val=df['values'].tolist())
+
+
+@app.route('/dashboard', methods=['GET', 'POST'])
+def dashboard():
+    name = request.form.get('name')
+    print(name)
+    session["keyword"] = name
+    countries = all_nations()
+    for x, y in zip(countries['id'], countries['name']):
+            if y == name:
+                _, get_title = get_data(x)
+
+    return render_template('dashboard.html', titles=get_title)
+
+
+    
 
 
 
--- fashlance/utils.py
+++ fashlance/utils.py
@@ -2,20 +2,40 @@ import wbdata
 import datetime
 import pandas as pd
 
-def get_data(indicator, country):
+def get_data(country):
+    get_indicators = ['SP.POP.TOTL', 'SP.DYN.LE00.IN', 
+    'SP.DYN.CBRT.IN', 'SP.POP.GROW', 'EN.POP.DNST', 'SP.URB.TOTL.IN.ZS',
+    'NY.GDP.MKTP.KD.ZG', 'SE.ADT.LITR.ZS', 'SL.UEM.TOTL.ZS']
     date_date = datetime.datetime(2010, 1, 1), datetime.datetime(2022, 1, 1)
-    data = wbdata.get_data(indicator, country=country, data_date=date_date)
-    df = pd.DataFrame()
-    year = []
-    values = []
-    for item in data:
-        year.append(item['date'])
-        values.append(item['value'])
-    df['year'] = year
-    df['values'] = values
-    sorted_df = df.sort_values(by=['year'], ascending=True)
-    
-    return sorted_df
+
+    title = []
+    info = []
+
+
+    for values in get_indicators:
+        data = wbdata.get_data(values, country=country, data_date=date_date)
+        my_dict = data[0]
+        values_view = my_dict.values()
+        value_iterator = iter(values_view)
+        first_value = next(value_iterator)
+        title.append(first_value['value'])
+
+        year = []
+        values = []
+        df = pd.DataFrame()
+        for item in data:
+            year.append(item['date'])
+            values.append(item['value'])
+        df['year'] = year
+        df['values'] = values
+        sorted_df = df.sort_values(by=['year'], ascending=True)
+        info.append(sorted_df)
+
+    new_df = pd.DataFrame()
+    new_df['title'] = title
+    new_df['info'] = info
+
+    return new_df, title
 
 def all_nations():
     countries = wbdata.search_countries('')
